ccbed989984c30d4913dc082723a5203a046cb08
Daniel Santos <danieldossantos1707@yahoo.com.br>
2018-03-23 10:56:23 -0300

    Added validation to type used in request

--- controllers/request_controller.rb
+++ controllers/request_controller.rb
@@ -5,12 +5,21 @@ class RequestController
   attr_accessor :headers, :body, :path
 
   def initialize(type, headers, body, path)
+    validate_type_request
+
     @type = type
     @headers = headers
     @body = body
     @path = path
   end
 
-  
+  private
+  def validate_type_request
+    value = @type.downcase.in?(['post', 'get', 'put', 'delete', 'patch'])
+
+    if value.eql?(false)
+      raise "Type informed [#{@type}] is incorrect. You have to use one of the followings types: post, get, put, delete, patch"
+    end
+  end
 
 end
