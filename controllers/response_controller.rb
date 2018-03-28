d9d4a90f052e6a44c572a1268e01e774593881d4
Hugo Silva <hugo.pereira.silva85@gmail.com>
2018-03-28 11:09:37 -0300

    Add new response fix

--- controllers/response_controller.rb
+++ controllers/response_controller.rb
@@ -1,11 +1,18 @@
-require '../../lib/requests'
 require 'json'
 
-def process_response(response)
-  return { body: JSON.parse(response.body), code: response.code, detail: response }
+class ResponseController
+
+def self.process_response(response)
+  json = { body: JSON.parse(response.body), code: response.code, detail: response }
+
+  p json[:body]
+
+  p instance_json_object(json[:body])
+
+  return instance_json_object(json[:body])
 end
 
-def instance_json_object(json)
+def self.instance_json_object(json)
     json_size = json.size
     response_object = OpenStruct.new(json)
 
@@ -18,13 +25,13 @@ def instance_json_object(json)
       if json_son.is_a? (Array)
         if json_son.first.is_a? (Hash)
           json_son.size.times do |position|
-            struct_json_son = instance_node_table_to_json(json_son[position])
+            struct_json_son = instance_json_object(json_son[position])
             instance_eval "response_object.#{attribute}[#{position}] = struct_json_son"
           end
         end
 
       elsif json_son.is_a? (Hash)
-        struct_json_son = instance_node_table_to_json(json_son)
+        struct_json_son = instance_json_object(json_son)
         instance_eval "response_object.#{attribute} = struct_json_son"
       end
 
