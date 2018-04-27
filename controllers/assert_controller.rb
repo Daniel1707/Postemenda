0879a535401fb2ac1715cfe41f785c8df8794a3a
Hugo Silva <hugo.pereira.silva85@gmail.com>
2018-04-27 16:11:49 -0300

    Frango robo

--- controllers/assert_controller.rb
+++ controllers/assert_controller.rb
@@ -2,29 +2,5 @@ require 'requests'
 
 class AssertController
 
-  attr_accessor :result, :expected,
-
-
-  def CamelLOIVEIO(fara=nil)
-    1 = 10
-    2 = 5
-
-    manga = []
-
-    if manga == 3 do
-      if manga == 3 do
-        if manga == 3 do
-          if manga == 3 do
-            if manga == 3 do
-              if manga == 3 do
-                if manga == 3 do
-                  p trouxa
-                end
-              end
-            end
-          end
-        end
-      end
-    end
-  end
+  attr_accessor :result, :expected
 end
