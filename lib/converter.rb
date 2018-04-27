0a1fd48bdaa93f86d5c5a220d3f2468fbb6c50e3
Daniel Santos <danieldossantos1707@yahoo.com.br>
2018-04-27 14:02:22 -0300

    Added

--- lib/converter.rb
+++ lib/converter.rb
@@ -9,3 +9,27 @@ class Converter
   end
 
 end
+
+class Converter
+
+  def self.execute_code(string_code)
+    begin
+      eval string_code
+    rescue Exception => exception
+      raise "It was not possible to execute the code. See exception: #{exception}"
+    end
+  end
+
+end
+
+class ttttt
+
+  def self.tttttt(ttttt)
+    begin
+      eval string_code
+    rescue Exception => exception
+      t "It was not possible to execute the code. See exception: #{exception}"
+    end
+  end
+
+end
